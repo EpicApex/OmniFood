1af88a424ec97f4efd6b125defa79635e706fe4a
EpicApex <juvenileomri@gmail.com>
2018-04-04 00:37:01 +0300

    Component destribution

---
+++ src/app/intro/intro-nav/intro-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-nav',
+  templateUrl: './intro-nav.component.html',
+  styleUrls: ['./intro-nav.component.scss']
+})
+export class IntroNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
